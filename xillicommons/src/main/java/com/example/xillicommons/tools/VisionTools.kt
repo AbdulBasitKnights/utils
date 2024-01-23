14f25f0c2c1e9fa272a9d660792432ecd2a22156
abdulbasit <abasit@terafort.com>
2024-01-23 15:32:47 +0500

    new Util function of share app with custom message

--- xillicommons/src/main/java/com/example/xillicommons/tools/VisionTools.kt
+++ xillicommons/src/main/java/com/example/xillicommons/tools/VisionTools.kt
@@ -1,11 +1,16 @@
 package com.example.xillicommons.tools
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import com.example.xillicommons.R
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -70,6 +75,71 @@ object VisionTools {
             // You can log the error here for debugging, if needed
             dateTimeString
         }
+    }
+    fun convertToEnglishDate(date: String): String {
+
+        return try {
+
+            val initDate = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(date)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val formatter = SimpleDateFormat("dd MMM YYYY", Locale.ENGLISH)
+                return formatter.format(initDate)
+            }
+            else{
+                val formatter = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH)
+                return formatter.format(initDate)
+            }
+
+
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            ""
+        }
+
+
+    }
+
+    fun timePicker(context: FragmentActivity?,theme:Int, callback: (String?) -> Unit) {
+        try {
+            val hour:Int?=0
+            val minute:Int?=0
+            val timePicker = hour?.let { it1 ->
+                try {
+                    minute?.let { it2 ->
+                        MaterialTimePicker.Builder()
+                            .setTimeFormat(TimeFormat.CLOCK_12H)
+                            .setTheme(theme)
+                            .setHour(it1)  // Set current hour
+                            .setMinute(it2)  // Set current minute
+                            .build()
+                    }
+                }
+                catch (e:Exception){
+                    minute?.let { it2 ->
+                        MaterialTimePicker.Builder()
+                            .setTimeFormat(TimeFormat.CLOCK_12H)
+                            .setHour(it1)  // Set current hour
+                            .setMinute(it2)  // Set current minute
+                            .build()
+                    }
+                }
+            }
+            timePicker?.addOnPositiveButtonClickListener {
+                val selectedHour = timePicker.hour
+                val selectedMinute = timePicker.minute
+                val amPm = if (selectedHour < 12) "AM" else "PM"  // Correct AM/PM logic
+                val displayHour = if (selectedHour == 0) 12 else selectedHour % 12
+
+                val formattedTime = String.format("%02d:%02d %s", displayHour, selectedMinute, amPm)
+                val time=formattedTime// Use 'text' property for efficiency
+                callback(time)
+            }
+            timePicker?.show(context?.supportFragmentManager!!, "timePicker")
+        }
+        catch (e:Exception){
+            //
+        }
+
     }
 
 }
